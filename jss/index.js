acb967b89966bcec7e8cc2fbe4e1d6d031157e9b
Akshat Pasbola <94235440+saujix@users.noreply.github.com>
2024-11-07 21:32:24 +0000

    Add files via upload

---
+++ jss/index.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.videoBlock').forEach(function(event){
+        document.querySelector(`#${event.id}`).addEventListener('mouseenter',function(){
+            document.querySelector(`#${event.id}`).play();
+        })
+})
+document.querySelectorAll('.videoBlock').forEach(function(event){
+        document.querySelector(`#${event.id}`).addEventListener('mouseleave',function(){
+            document.querySelector(`#${event.id}`).pause();
+        })
+})
